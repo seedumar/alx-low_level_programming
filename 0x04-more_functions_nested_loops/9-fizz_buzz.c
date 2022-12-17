d86dfa0c6440553b800e2fe517ad45cb1081486a
seedumar <seedumar1999@gmail.com>
2022-12-16 17:23:14 -0800

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,29 +7,19 @@
 
 int main(void)
 {
-	int n;
+	int start = 1, end = 100;
 
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+	while (start <= 100)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-		{
-			printf(" FizzBuzz")
-		}
-		else if (n % 3 == 0)
-		{
-			printf(" Fizz");
-		}
-		else if (n % 5 == 0)
-		{
-			printf(" Buzz");
-		}
-		else
-		{
-			print(" %d", n);
-		}
+		if (start % 3 == 0)
+			printf("Fizz");
+		if (start % 5 == 0)
+			printf("Buzz");
+		if (start % 3 != 0 && start % 5 != 0)
+			printf("%d", start);
+
+		putchar(start == end ? '\n' : ' ');
+		start++;
 	}
-	printf ("\n");
 	return (0);
 }
