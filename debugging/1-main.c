4d3cdb76ca0fa26d091ad1ef4717b922719568f4
seedumar <seedumar1999@gmail.com>
2022-12-16 16:19:53 -0800

    My commit

---
+++ debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	print("Infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * _putcha(i):
+	 * }
+	 */
+	printf("infinite loop avoided: \\o/\n");
+
+	return (0);
+}
