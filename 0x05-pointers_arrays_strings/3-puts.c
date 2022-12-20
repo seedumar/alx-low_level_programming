dc241da22f827c345fc76dc726b869ca3dcbb0cc
seedumar <seedumar1999@gmail.com>
2022-12-19 18:36:41 -0800

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 nclude "main.h"
 
 /**
- * _put - Write a function that prints a string. followed by a new line, to stdout
+ * _puts - Write a function that prints a string. followed by a new line, to stdout
  * @str: This is my input string
  */
 
-void _put(char *str)
+void _puts(char *str)
 {
 	int index;
 
 	for (index = 0; str[index] != '\0'; index++)
 	{
-		_putchar(str[index);
+		_putchar(str[index]);
 	}
 	_putchar('\n');
 }
