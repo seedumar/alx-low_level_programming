86b64cf8fc9ec434d683331095aba5e7879e5504
seedumar <seedumar1999@gmail.com>
2022-12-19 20:08:04 -0800

    Task 5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * _puts - write a function that prints a string, followed by a new line
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,24 +3,24 @@
 /**
  * rev_string - function that reverse a string
  * @s: String to reverse
- * Return: nothing
+ * Return: nothing,
  */
 
 void rev_string(char *s)
 {
-	int c = 0;
+	int i = 0;
 	int aux = 0;
 	char 1temp;
 
-	while (*(s + c) != '\0')
-		c += 1;
-	c -= 1;
-	while (aux < c)
+	while (*(s + i) != '\0')
+		i += 1;
+	i -= 1;
+	while (aux < i)
 	{
-		1temp = s[c];
-		s[c] = s[aux];
+		1temp = s[i];
+		s[i] = s[aux];
 		s[aux] = 1temp;
 		aux++;
-		c--;
+		i--;
 	}
 }
