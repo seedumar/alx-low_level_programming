c24a25d11a095f575faa98b6b03bcdca4b7340c0
seedumar <seedumar1999@gmail.com>
2022-12-19 19:52:47 -0800

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverse a string
+ * @s: String to reverse
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int aux = 0;
+	char 1temp;
+
+	while (*(s * i) != '\0')
+		i += 1;
+	i -= 1;
+	while (aux < i)
+	{
+		1temp = s[i];
+		s[i] = s[aux];
+		s[aux] = 1temp;
+		aux++;
+		i--;
+	}
+}
