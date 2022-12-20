cf824f56cb39a089c85f8c4a4fe2ca84ed16517a
seedumar <seedumar1999@gmail.com>
2022-12-20 12:39:25 -0800

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
+ * _strcpy - function that copies the string
  *
  * @dest: pointer to destination char
  * @src: pointer to source char
  * Return: char
  */
 
-char *strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
 
