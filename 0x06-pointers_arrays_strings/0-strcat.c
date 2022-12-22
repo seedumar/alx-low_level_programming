ba1eba79ad3567233cd623172f3c2b455f18bdf8
seedumar <seedumar1999@gmail.com>
2022-12-22 15:41:40 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminat
  *          null byte, to the end of the string pointed to by @dest.
@@ -10,22 +9,11 @@
  */
 char *strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int index = 0, dest_len = 0;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-
-	dest[i] = '\0';
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index++];
 	return (dest);
 }
