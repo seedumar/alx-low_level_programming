2a0eec0ff36f249ca6b2ea172538334c0a2b8678
seedumar <seedumar1999@gmail.com>
2022-12-23 15:08:35 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,29 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, diff = 0;
+	int i;
 
-	while (i)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-			break;
-		else if (s1[i] == '\0')
+		if (s1[i] != s2[i])
 		{
-			diff = s2[i];
-			break;
+			return (s1[i] - s2[i]);
 		}
-		else if (s2[i] == '\0')
-		{
-			diff = s1[i];
-			break;
-		}
-		else if (s2[i] != s2[i])
-		{
-			diff = (s1[i] - s2[i])
-			break;
-		}
-		else
-			i++;
+	i++;
 	}
-	return (diff);
+	return (0);
 }
