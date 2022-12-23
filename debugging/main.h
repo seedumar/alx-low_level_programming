c9da14c580c0f29404eb099be026c88d41391945
seedumar <seedumar1999@gmail.com>
2022-12-22 16:31:52 -0800

    My commit

--- debugging/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_h
-#include <stdio.h>
-void positive_or_negative(int 1);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-
-#endif
