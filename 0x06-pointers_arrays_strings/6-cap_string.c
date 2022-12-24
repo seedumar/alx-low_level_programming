0272135b6516f8949dc8bf1281aaa8c3d482be57
seedumar <seedumar1999@gmail.com>
2022-12-23 16:04:59 -0800

    Task 7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,11 +24,8 @@ char *cap_string(char *str)
 		    str[i - 1] == '(' ||
 		    str[i - 1] == ')' ||
 		    str[i - 1] == '{' ||
-		    str[i - 1] == '}' ||
-		    i == 0)
+		    str[i - 1] == '}')
 			str[i] == 32;
-		i++;
 	}
-
 	return (str);
 }
