ec909eb0769cb00728f9f1c3c483befead2014e9
seedumar <seedumar1999@gmail.com>
2023-01-11 16:34:05 -0800

    My commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,41 +1,43 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers
- * @width: int 1
- * @height: int 2
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the array.
+ * @height: height of the array.
  *
- * Return: pointer
+ * Return: pointer of an array of integers
  */
 int **alloc_grid(int width, int height)
 {
-	int **_new_height;
-	int i = 0, j = 0;
+	int **gridout;
+	int i, j;
 
-	if (width <= 0 || height <= 0)
+	if (width < 1 || height < 1)
 		return (NULL);
-	_new_height = malloc(height * sizeof(int *));
-	if (_new_height == NULL)
+
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
+	{
+		free(gridout);
 		return (NULL);
+	}
+
 	for (i = 0; i < height; i++)
 	{
-		_new_height[i] = malloc(width * sizeof(int));
-		if (_new_height[i] == NULL)
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
 		{
-			for (; i >= 0; i--)
-			{
-				free(_new_height[i]);
-			}
-			free(_new_height);
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
+			free(gridout);
 			return (NULL);
 		}
 	}
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			_new_height[i][j] = 0;
-		}
-	}
 
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
+
+	return (gridout);
+}
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,20 +1,19 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * free_grid - function that frees a 2 dimensional grid previously created
- * @grid: pointer
- * @height: altura
+ * free_grid - frees a 2 dimensional grid.
+ * @grid: multidimensional array of integers.
+ * @height: height of the grid.
  *
+ * Return: no return
  */
-
 void free_grid(int **grid, int height)
 {
-	int cont;
-
-	for (cont = 0; cont < height; cont++)
+	if (grid != NULL && height != 0)
 	{
-		free(grid[cont]);
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
 	}
-	free(grid);
 }
