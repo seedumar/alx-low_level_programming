761b6043348bbf48429dc5f23688f1f898a9f8a6
seedumar <seedumar1999@gmail.com>
2023-01-18 16:56:20 -0800

    My commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,48 +1,28 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
-<<<<<<< HEAD
- * get_op_func - ...
- * @s: ...
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
  *
- * Return: ...
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
  */
-=======
-  * get_op_func - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-<<<<<<< HEAD
-	{ "/", op_div }
-=======
-	{ "/", op_div },
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i = 0;
 
-	while (i < 5)
-	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-<<<<<<< HEAD
-		
-=======
-
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
 
-	return (0);
+	return (ops[i].f);
 }
