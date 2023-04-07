0d7e3d835fa59b71a2fb2921b1b4964a5f1cf3e5
seedumar <seedumar1999@gmail.com>
2023-04-07 12:37:30 +0000

    Hash Table

---
+++ 0x1A-hash_tables/0-hash_create.c
@@ -0,0 +1,23 @@
+/*
+* Author: Umar Saeed umar
+*/
+
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hasht;
+	unsighned long int a;
+
+	hasht = malloc(sizeof(hash_table_t));
+	if (hasht == NULL)
+		return (NULL);
+	hasht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (hasht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		hasht->array[i] = NULL;
+
+	return (hasht);
+}
