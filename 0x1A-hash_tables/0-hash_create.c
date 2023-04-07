4c697fb11b692b462ee281d7018403da711365db
seedumar <seedumar1999@gmail.com>
2023-04-07 12:43:58 +0000

    Hash Table

--- 0x1A-hash_tables/0-hash_create.c
+++ 0x1A-hash_tables/0-hash_create.c
@@ -7,7 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hasht;
-	unsighned long int a;
+	unsighned long int i;
 
 	hasht = malloc(sizeof(hash_table_t));
 	if (hasht == NULL)
